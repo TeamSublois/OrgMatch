0a8c7d009847e581c2d654fea2ca89de487255ad
Sean Nam <namsangwoo1@gmail.com>
2018-02-20 00:28:48 -0800

    Created Volunteer and Organization models

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,16 +1,23 @@
 from django.db import models
 
 class Volunteer(models.Model):
-
   first_name = models.CharField(max_length=20)
   last_name = models.CharField(max_length=30)
   bio = models.CharField(max_length=140)
-  profile_picture = models.ImageField(upload_to='uploads/', max_length=100)
-  # interest_list
-  # org_list
+  profile_picture = models.ImageField(upload_to='mainapp/static/mainapp/images/', max_length=100)
   city = models.CharField(max_length=30)
   state = models.CharField(max_length=2)
-  
-  # def hash_profile_pic_url(self):
-  #   return self
 
+  def __str__(self):
+      return self.first_name + " " + self.last_name
+
+
+class Organization(models.Model):
+  name = models.CharField(max_length=30)
+  bio = models.CharField(max_length=140)
+  profile_picture = models.ImageField(upload_to='mainapp/static/mainapp/images/', max_length=100)
+  city = models.CharField(max_length=30)
+  state = models.CharField(max_length=2)
+
+  def __str__(self):
+      return self.name
