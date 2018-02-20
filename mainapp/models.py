50c28840f3b0a1fdd1dff0a26836ca210c712b01
Sean Nam <namsangwoo1@gmail.com>
2018-02-20 01:07:16 -0800

    Updated models

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,12 +1,15 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 class Volunteer(models.Model):
   first_name = models.CharField(max_length=20)
   last_name = models.CharField(max_length=30)
   bio = models.CharField(max_length=140)
-  profile_picture = models.ImageField(upload_to='mainapp/static/mainapp/images/', max_length=100)
+  profile_picture = models.ImageField(upload_to='mainapp/static/mainapp/images/', max_length=100, blank=True)
   city = models.CharField(max_length=30)
   state = models.CharField(max_length=2)
+  category_list = ArrayField(models.CharField(default="", max_length=20, blank=True))
+  fav_org_list = ArrayField(models.CharField(default="", max_length=20, blank=True))
 
   def __str__(self):
       return self.first_name + " " + self.last_name
@@ -18,6 +21,9 @@ class Organization(models.Model):
   profile_picture = models.ImageField(upload_to='mainapp/static/mainapp/images/', max_length=100)
   city = models.CharField(max_length=30)
   state = models.CharField(max_length=2)
+  categories = ArrayField(models.CharField(max_length=20, blank=True, default="nonprofit"))
+  time_commitement = ArrayField(models.IntegerField(default=10), size=2)
+  events = ArrayField(models.CharField(default="", max_length=20, blank=True))
 
   def __str__(self):
       return self.name
