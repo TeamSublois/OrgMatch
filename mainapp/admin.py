019bca0a771252862d1d35bdfad9ea2effe8cf20
Sean Nam <namsangwoo1@gmail.com>
2018-02-26 01:59:24 -0800

    db model update

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Volunteer, Organization
+from .models import Volunteer, Organization, State, Category
 
 admin.site.register(Volunteer)
 admin.site.register(Organization)
+admin.site.register(State)
+admin.site.register(Category)
