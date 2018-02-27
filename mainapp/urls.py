8c6c10a620594b7842dd32d9ef62705272a2e8fd
Wesley <wec137@ucsd.edu>
2018-02-27 13:17:05 -0800

    made everything functional now

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'mainapp'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('results.html', views.results, name='results')
+    path('index/', views.index, name='index'),
+    path('recommended/', views.recommended, name='recommended')
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -7,9 +7,9 @@ def index(request):
 	template = loader.get_template('mainapp/index.html')
 	return HttpResponse(template.render())
 
-def results(request):
+def recommended(request):
 
 	all_organizations_list = Organization.objects.order_by('city')[:5]
 	context = {'all_organizations_list': all_organizations_list,}
 	
-	return render(request, 'mainapp/results.html', context)
+	return render(request, 'mainapp/recommended.html', context)
