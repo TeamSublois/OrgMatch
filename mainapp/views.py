6d6e2a3de2db0e94691c9dc8001190af16a3166b
Benson <blbudima@ucsd.edu>
2018-02-26 18:04:11 -0800

    help

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,8 +5,7 @@ from django.shortcuts import render
 from .models import Category
 
 def index(request):
-    template = loader.get_template('mainapp/index.html')
-    return HttpResponse(template.render())
     all_categories_list = Category.objects.order_by('name')[:5]
     context = {'all_categories_list':all_categories_list}
+    
     return render(request, 'mainapp/index.html', context)
