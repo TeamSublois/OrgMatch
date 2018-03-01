d562442c39595f401ea950f96eddcc217b06264d
Benson Budiman <blbudima@ucsd.edu>
2018-03-01 13:25:23 -0800

    home and category succesfully(?) linked to database - need to fix html and css

--- mainapp/views.py
+++ mainapp/views.py
@@ -9,3 +9,10 @@ def index(request):
     context = {'all_categories_list':all_categories_list}
     
     return render(request, 'mainapp/home.html', context)
+
+
+def category(request):
+	all_categories_list = Category.objects.order_by('name')[:5]
+	context = {'all_categories_list':all_categories_list}
+
+	return render(request, 'mainapp/category.html', context)
