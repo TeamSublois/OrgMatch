d0228db697596e4486505d13a12a21ea9c44f355
Benson Budiman <blbudima@ucsd.edu>
2018-03-01 20:08:51 -0800

    attempted to better style home using css, but why aren't the categories centering?

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,14 +5,14 @@ from .models import Category, Organization
 
 
 def index(request):
-    all_categories_list = Category.objects.order_by('name')[:5]
+    all_categories_list = Category.objects.order_by('name')[:3]
     context = {'all_categories_list':all_categories_list}
     
     return render(request, 'mainapp/home.html', context)
 
 
 def category(request):
-	all_categories_list = Category.objects.order_by('name')[:5]
+	all_categories_list = Category.objects.order_by('name')[:1000]
 	context = {'all_categories_list':all_categories_list}
 
 	return render(request, 'mainapp/category.html', context)
